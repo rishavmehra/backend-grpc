4c209a39391ab2fd7e9716f9505330d8ad457241
Rishav Mehra <rishavmehra61@gmail.com>
2024-08-31 18:27:56 +0530

    enviroment(.env) variable setup using viper

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -7,19 +7,24 @@ import (
 	"testing"
 
 	_ "github.com/lib/pq"
+	"github.com/rishavmehra/backend-grpc/util"
 )
 
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://root:hide1337@localhost:5432/simple_bank?sslmode=disable"
-)
+// const (
+// 	dbDriver = "postgres"
+// 	dbSource = "postgresql://root:hide1337@localhost:5432/simple_bank?sslmode=disable"
+// )
 
 var testQuries *Queries
 var testDB *sql.DB
 
 func TestMain(m *testing.M) {
 	var err error
-	testDB, err = sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig("../../")
+	if err != nil {
+		log.Fatal("Cannot Load configuration: ", err)
+	}
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("Not able to make connection with Database.", err)
 	}
